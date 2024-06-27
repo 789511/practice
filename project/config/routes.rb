6989eee62177167dc2f6a2a8d5c66b36a7ec5089
pivolga_v <myskov.slava@yandex.ru>
2024-06-27 15:15:03 +0300

    добавлен функционал devise

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  devise_for :users
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   namespace :admin do
@@ -7,6 +10,7 @@ Rails.application.routes.draw do
       root to: "games#index"
     end
   resources :games
+  resource :profile, only: [:show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
